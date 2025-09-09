73167a262e143fa62ecaede5402b56fe1eb39cfb
Bruno <brunos.souza2@gmail.com>
2025-09-09 13:32:48 -0300

    fix: Corrige o casing do header X-Tenant no middleware de tenant

--- backend/src/tenant-middleware/tenant-middleware.middleware.ts
+++ backend/src/tenant-middleware/tenant-middleware.middleware.ts
@@ -7,10 +7,14 @@ export class TenantMiddleware implements NestMiddleware {
 
   use(req: Request, res: Response, next: NextFunction) {
     this.logger.debug('TenantMiddleware called');
-    const domain = req.headers['X-Tenant']; // Ex: 'tenant1.example.com'
+
+    // Apesar de HTTP headers serem case-insensitive, alguns ambientes podem alterar o casing dos headers.
+    // Esse parece ser o caso aqui com o NestJS.
+    const domain = req.headers['x-tenant']; // Ex: 'web.tenant1.com'
 
     if (typeof domain !== 'string') {
-      this.logger.warn('Host header is missing or not a string');
+      this.logger.warn('X-Tenant header is missing or not a string');
+      //TODO: Aplique a lógica padrão ou retorne um erro, conforme necessário.
       next();
       return;
     }
@@ -18,7 +22,7 @@ export class TenantMiddleware implements NestMiddleware {
     this.logger.debug(`Extracted domain: ${domain}`);
     if (domain) {
       this.logger.debug(`Setting X-Tenant header to: ${domain}`);
-      req.headers['X-Tenant'] = domain;
+      req.headers['x-tenant'] = domain;
     }
 
     next();
